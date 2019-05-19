86ff920b54dc3c62f9f1143f25b92ba90210deef
SoniEx2 <endermoneymod@gmail.com>
2019-05-19 11:14:49 -0300

    Update 2019-05-19

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-./wrapper.bash cron-target 125baefaade696119262cd7ae5127c4265a59edc > project/125baefaade696119262cd7ae5127c4265a59edc/index.html
-cp ganarchy/index.js ./index.js
-git add .
-git commit -a -m "Update $(date -I -u)"
-git push
+./wrapper.bash cron-target 125baefaade696119262cd7ae5127c4265a59edc > project/125baefaade696119262cd7ae5127c4265a59edc/index.html || exit 1
+cp ganarchy/index.js ./index.js || exit 1
+git add . || exit 1
+git commit -a -m "Update $(date -I -u)" || exit 1
+git push || exit 1
