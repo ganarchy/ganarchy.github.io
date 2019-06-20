c926feff5ac083e5b62d27c4525b75445749d639
SoniEx2 <endermoneymod@gmail.com>
2019-06-20 17:10:36 -0300

    Update 2019-06-20T20:10:36+00:00

--- update.sh
+++ update.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
-./wrapper.bash cron-target 125baefaade696119262cd7ae5127c4265a59edc > project/125baefaade696119262cd7ae5127c4265a59edc/index.html || exit 1
+mkdir -p ./private/ganarchy/
+mkdir -p ./.well-known/ganarchy/
+
+# load remote configs (if any)
+(cd ./private/ && exec ./fetch)
+
+# if expansion fails, it won't print to stdout and config.toml will be empty. just ignore stderr.
+./wrapper.bash merge-configs ./private/*.toml 2>/dev/null > ./private/ganarchy/config.toml
+
+# read and process projects
+projects=$(./wrapper.bash cron-target project-list) || exit 1
+printf '%s\n' "$projects" | while IFS= read -r project; do
+	mkdir -p ./project/"$project"/ || exit 1
+	./wrapper.bash cron-target "$project" > ./project/"$project"/index.html || exit 1
+done
+
+# generate index, config, etc
+./wrapper.bash cron-target index > ./index.html || exit 1
+./wrapper.bash cron-target config > ./config.toml || exit 1
 cp ganarchy/index.js ./index.js || exit 1
+
+# push to github pages
 git add . || exit 1
 git commit -a -m "Update $(date -Iseconds -u)" || exit 1
 git push || exit 1
--- wrapper.bash
+++ wrapper.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-export XDG_DATA_HOME=./data
-export XDG_CACHE_HOME=./cache
+export XDG_DATA_HOME="$(pwd)"/data
+export XDG_CACHE_HOME="$(pwd)"/cache
+export XDG_CONFIG_HOME="$(pwd)"/config
+export XDG_CONFIG_DIRS="$(pwd)"/private
 source venv/bin/activate
 ./ganarchy/ganarchy.py "$@"
