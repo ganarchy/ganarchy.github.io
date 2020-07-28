2444d62ef5b2a7e259f25a6e7daa6812bbc96a26
SoniEx2 <endermoneymod@gmail.com>
2020-07-27 22:27:00 -0300

    Update 2020-07-28T01:27:00+00:00

--- setup-env.bash
+++ setup-env.bash
@@ -3,5 +3,7 @@ export XDG_DATA_HOME="$(pwd)"/data
 export XDG_CACHE_HOME="$(pwd)"/cache
 export XDG_CONFIG_HOME="$(pwd)"/config
 #export XDG_CONFIG_DIRS="$(pwd)"/private
-source venv/bin/activate
-python -m ganarchy "$@"
+
+python -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
--- update.sh
+++ update.sh
@@ -1,24 +1,5 @@
 #!/bin/sh
-mkdir -p ./private/ganarchy/
-mkdir -p ./.well-known/ganarchy/
-
-# load remote configs (if any)
-(cd ./private/ && exec ./fetch)
-
-# if expansion fails, it won't print to stdout and config.toml will be empty. just ignore stderr.
-./wrapper.bash merge-configs ./private/*.toml 2>/dev/null > ./private/ganarchy/config.toml
-
-# read and process projects
-projects=$(./wrapper.bash cron-target project-list) || exit 1
-printf '%s\n' "$projects" | while IFS= read -r project; do
-	mkdir -p ./project/"$project"/ || exit 1
-	./wrapper.bash cron-target "$project" > ./project/"$project"/index.html || exit 1
-done
-
-# generate index, config, etc
-./wrapper.bash cron-target index > ./index.html || exit 1
-./wrapper.bash cron-target config > ./config.toml || exit 1
-cp ganarchy/index.js ./index.js || exit 1
+./wrapper.bash run-once . || exit 1
 
 # push to github pages
 git add . || exit 1
