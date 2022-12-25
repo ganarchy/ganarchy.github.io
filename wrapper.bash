7a21fbe9c52e56ee1fee0964101e3a4db16e3f89
SoniEx2 <endermoneymod@gmail.com>
2022-12-24 22:51:53 -0300

    Update 2022-12-25T01:51:52+00:00

--- wrapper.bash
+++ wrapper.bash
@@ -2,6 +2,7 @@
 export XDG_DATA_HOME="$(pwd)"/data
 export XDG_CACHE_HOME="$(pwd)"/cache
 export XDG_CONFIG_HOME="$(pwd)"/config
-#export XDG_CONFIG_DIRS="$(pwd)"/private
+export XDG_CONFIG_DIRS="$(pwd)"/private:"${XDG_CONFIG_DIRS:-/etc/xdg}"
+export GIT_TERMINAL_PROMPT=0
 source venv/bin/activate
 python -m ganarchy "$@"
